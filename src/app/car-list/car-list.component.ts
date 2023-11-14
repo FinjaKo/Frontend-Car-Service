2504b4d0524e53a1b654c1af0cd8322b9f2674b9
Finja Koopmann <finja.koopmann@emeal.nttdata.com>
2023-11-14 10:48:46 +0100

    car-list component (old trys comment out)

---
+++ src/app/car-list/car-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {Car} from '../car'
+import {CarService} from '../car.service'
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css']
+})
+
+export class CarListComponent implements OnInit {
+  cars: Car[];
+
+  constructor(private carService: CarService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  private getCars() {
+    this.carService.getCarList().subscribe(data => {
+      this.cars = data;
+    });
+  }
+
+  carDetails(fin: string) {
+    this.router.navigate(['car-details', fin]);
+  }
+
+  updateCar(fin: string) {
+    this.router.navigate(['update-car', fin]);
+  }
+
+  deleteCar(fin: string) {
+    this.carService.deleteCar(fin).subscribe(data => {
+      console.log(data);
+      this.getCars();
+    })
+  }
+}
